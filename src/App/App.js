479fff90d3db26c044bbea3c721dc3ffee1bce18
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-02 13:44:14 -0500

    Add make resrvation form and functionality

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import { getReservations } from '../apiCalls';
 import ReservationCards from "../ReservationCards/ReservationCards"
-
+import Form from "../Form/Form"
 function App() {
   const [reservations, setReservations] = useState([]);
   const fetchData = () => {
      getReservations().then(data => {
       setReservations(data)
      });
+  }
+
+  const addReservation = (newReservation) => {
+    setReservations([...reservations, newReservation])
   }
   useEffect(() => {
     fetchData()
@@ -17,7 +21,7 @@ function App() {
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
-        {/* <Form /> */}
+        <Form addReservation={addReservation}/>
       </div>
       <div className='resy-container'>
         <ReservationCards reservations={reservations}/>
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,13 +1,63 @@
 import { useState } from "react";
-
-function Form() {
+function Form({addReservation}) {
   const [form, setForm] = useState({
     name: "",
     date: "",
     time: "",
     number: 0
   })
-  function setDataToForm = {
-    
+  function setDataToForm(event) {
+    const newFormData = {...form}
+     newFormData[event.target.name] = event.target.value;
+     setForm(newFormData);
+     console.log(form)
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    addReservation(form)
+  }
+  return(
+    <form className="input-reservation-form">
+      <label>Name:
+      <input
+      name="name"
+      type="text"
+      placeholder="name"
+      onChange={(event) => {setDataToForm(event)}}
+      value={form.name}
+      />
+      </label>
+      <label>Date:
+      <input
+      name="date"
+      type="text"
+      placeholder="date"
+      onChange={(event) => {setDataToForm(event)}}
+      value={form.date}
+      />
+      </label>
+      <label>Time:
+      <input
+      name="time"
+      type="text"
+      placeholder="time"
+      onChange={(event) => {setDataToForm(event)}}
+      value={form.time}
+      />
+      </label>
+      <label>Number:
+      <input
+      name="number"
+      type="text"
+      placeholder="number"
+      onChange={(event) => {setDataToForm(event)}}
+      value={form.number}
+      />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Make Reservation!</button>
+    </form>
+  )
 }
+
+export default Form;
